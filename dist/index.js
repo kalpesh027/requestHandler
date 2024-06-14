da3576d7ef4fb04aa6a6a3a048c079f1afaef7cb
Kalpesh Ghodekar <kalpeshghodekar2004@gmail.com>
2024-06-14 14:53:55 +0530

    #intial commit

---
+++ dist/index.js
@@ -0,0 +1,36 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const aws_sdk_1 = require("aws-sdk");
+const s3 = new aws_sdk_1.S3({
+    accessKeyId: "6e98aa3ba003d2c279e79711ddbaec88",
+    secretAccessKey: "ba8c1699ef36e159ce70203f14c77593ac2bf2d66e5f0172cc6a731135ea489b",
+    endpoint: "https://964cc39b87cffa873b9b8c1d162da524.r2.cloudflarestorage.com"
+});
+const app = (0, express_1.default)();
+app.get("/*", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    // id.host.com
+    const host = req.hostname;
+    const id = host.split(".")[0];
+    const filePath = req.path;
+    const contents = yield s3.getObject({
+        Bucket: "vercel",
+        Key: `dist/${id}${filePath}`
+    }).promise();
+    const type = filePath.endsWith("html") ? "text/html" : filePath.endsWith("css") ? "text/css" : "application/javascript";
+    res.set("Content-Type", type);
+    res.send(contents.Body);
+}));
+app.listen(3001);
